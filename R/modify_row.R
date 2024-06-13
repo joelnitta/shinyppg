f04bc24fcccf3bc42dccabc298f0452b25b315d3
joelnitta <joelnitta@gmail.com>
2024-06-14 08:49:32 +0900

    Add undo func. Fixes #9

--- R/modify_row.R
+++ R/modify_row.R
@@ -70,6 +70,8 @@ modify_row_server <- function(
             parentNameUsageID = null_if_blank(input$parentNameUsageID),
             parentNameUsage = null_if_blank(parentNameUsage())
           )
+          # Save patch for undoing
+          save_patch(data_original = ppg(), data_changed = updated_data)
           ppg(updated_data)
         },
         error = function(e) {
--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -13,3 +13,32 @@ test_that("initial validation works", {
     FALSE
   )
 })
+
+test_that("undo works", {
+  dwctaxon::dct_options(
+    user_name = "me",
+    user_id = "123",
+    stamp_modified_by = TRUE,
+    stamp_modified_by_id = TRUE,
+    extra_cols = c("modifiedBy", "modifiedByID")
+  )
+  ppg <- ppg_small
+  ppg_mod_1 <- ppg |>
+    dwctaxon::dct_modify_row(
+      scientificName = "Loxogramme antrophyoides (Baker) C. Chr.",
+      taxonomicStatus = "synonym",
+      parentNameUsage = "Cyathea meridensis H. Karst."
+    )
+  save_patch(ppg, ppg_mod_1)
+  ppg_mod_2 <- ppg_mod_1 |>
+    dwctaxon::dct_add_row(scientificName = "me")
+  save_patch(ppg_mod_1, ppg_mod_2)
+  recoverd_ppg_mod_1 <- undo_change(ppg_mod_2)
+  expect_equal(
+    ppg_mod_1,
+    recoverd_ppg_mod_1
+  )
+  recovered_ppg <- undo_change(ppg_mod_1)
+  expect_equal(ppg, recovered_ppg)
+  dwctaxon::dct_options(reset = TRUE)
+})
